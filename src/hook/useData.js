37ccf0bb386a0da0c0fa6f7b75f7d0c8f80fc5db
lersu <89482077+Iersu@users.noreply.github.com>
2021-09-30 16:58:43 +0200

    Update useData.js

--- src/hook/useData.js
+++ src/hook/useData.js
@@ -2,8 +2,8 @@
 
 export  function UseData() {
 
-    const CLIENT_ID = "X0RZXBQWBVGLSYFH522PC00HTZYO2GHVSZPGH03H0AYO1L4O"
-    const CLIENT_SECRET = "0QHSOXVMKDBXHQ1LKQ2YNPF5NP2GM23THJAPZHUSLLXRYIFK"
+    const CLIENT_ID = CLIENT_ID
+    const CLIENT_SECRET = CLIENT_SECRET
 
     const fetchShopId = ( lati, long) => {
         return  fetch(`https://api.foursquare.com/v2/venues/search?ll=${lati},${long}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&categoryId=4bf58dd8d48988d1e0931735&v=20212509&limit=5&radius=15000`)
